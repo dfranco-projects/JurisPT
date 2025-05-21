6701a302b54892b00a801135a6e62b5685e9f0e8
Daniel Franco <daniel.franco.inbox@gmail.com>
2025-05-22 00:13:57 +0100

    Updated scraper.py

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,37 +1,75 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Playwright, sync_playwright, expect
 
 class LawScraper:
-    def __init__(self):
+    def __init__(self) -> None:
         self.url = "https://diariodarepublica.pt"
 
-    def fetch_law_document(self, law_id):
-        with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True)
-            page = browser.new_page()
+    def fetch_law_doc(self, playwright: Playwright, law_id) -> str:
+            """
+            Fetches the law document from the website.
 
-            # open search page
-            page.goto(f"{self.url}/dr/pesquisa")
+            Args:
+                playwright (Playwright): playwright instance.
+                law_id (str): ID of the law to fetch.
 
-            # search by law id
-            page.fill('input[placeholder="Pesquisar"]', law_id)
-            page.keyboard.press('Enter')
+            Returns:        
+                str: content of the law document.
+            """
+            browser = playwright.firefox.launch(headless=False)
+            context = browser.new_context()
+            page = context.new_page()
+            page.goto(self.url)
 
-            # wait until results load
-            page.wait_for_selector('a.title')
+            # search
+            page.get_by_placeholder("O que procura?").fill(law_id)
+            page.get_by_role("button", name="Icon magnifying").click()
 
-            # open first result
-            page.click('a.title')
-            page.wait_for_load_state('networkidle')
+            # wait for popup and open it
+            # page.locator("#b2-Pesquisa").get_by_text(f"{law_id}", exact=True).click()
+            # page.locator("#b2-b2-b3-l3-240_0-ListItem2").click()
 
-            # get rendered html
-            content = page.content()
+            # result_locator = page.locator("#b2-b2-b3-l3-240_0-ListItem2")
+            # result_locator.wait_for(state="visible", timeout=5000)
+            # result_locator.hover()
+
+            # print("Before click URL:", page.url)
+            # result_locator.click()
+            # page.wait_for_load_state("networkidle")
+            # print("After click URL:", page.url)
+
+
+            #######################
+            
+            with page.expect_popup() as popup_info:
+                page.get_by_role("link", name=f"{law_id} -").click()
+            law_page = popup_info.value
+            law_page.wait_for_load_state()
+
+            ########################
+
+
+            # wait for the content to load (5 sec)
+            law_page.wait_for_load_state()
+            law_page.wait_for_selector(".texto.int-links", timeout=5_000)
+
+            # get law content
+            content = law_page.locator(".texto.int-links").inner_text()
+
+            # close the browser
             browser.close()
 
-            print(f"[✓] fetched document for {law_id}")
+            return content
+    
+    def run_scraper(self, law_id) -> None:
+        """
+        Runs the scraper to fetch the law document.
+
+        Args:
+            law_id (str): ID of the law to fetch.
+        """
+        with sync_playwright() as playwright:
+            content = self.fetch_law_doc(playwright, law_id)
             return content
         
 if __name__ == "__main__":
-
-    scraper = LawScraper()
-    html = scraper.fetch_law_document("Decreto-Lei n.º 90-C/2022")
-    print(html[:1500])
+    pass
