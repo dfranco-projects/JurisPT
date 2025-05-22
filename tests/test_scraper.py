17109bc89cf208abe47f09ed1f9bb4ddda42742b
Daniel Franco <daniel.franco.inbox@gmail.com>
2025-05-22 01:20:22 +0100

    Updated test_scraper.py

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -6,10 +6,7 @@ def test_law_scraper_output() -> None:
     Test the output of the law scraper for Decreto-Lei n.º 90-C/2022.
     """
     scraper = LawScraper()
-    content = scraper.run_scraper( "Decreto-Lei n.º 90-C/2022")
-
-    with open("/tmp/test_scraper_decreto_lei_90C_22.txt", "w", encoding="utf-8") as f:
-        f.write(content)
+    content = scraper.run_test_scraper("Decreto-Lei n.º 90-C/2022")
 
     assert "Decreto-Lei n.º 90-C/2022" in content
     assert "programa Porta 65" in content
@@ -21,11 +18,20 @@ def test_law_scraper_lei_37_2025() -> None:
     Test the output of the law scraper for Lei n.º 37/2025.
     """
     scraper = LawScraper()
-    content = scraper.run_scraper("Lei n.º 37/2025")
-
-    with open("/tmp/test_scraper_lei_37_25.txt", "w", encoding="utf-8") as f:
-        f.write(content)
+    content = scraper.run_test_scraper("Lei n.º 37/2025")
 
     assert "Lei n.º 37/2025" in content
     assert "Alteração à lei de proteção de crianças e jovens em perigo" in content
     assert "Manter contacto com a criança e jovem após a cessação" in content
+
+def test_law_scraper_dl_446_85() -> None:
+    """
+    Test the output of the law scraper for Decreto-Lei n.º 446/85.
+    """
+    scraper = LawScraper()
+    content = scraper.run_test_scraper("Decreto-Lei n.º 446/85")
+
+    assert "Decreto-Lei n.º 446/85" in content
+    assert "celeridade e de precisão, a existência de monopólios, oligopólios" in content
+    assert "desequilíbrio de prestações gravemente atentatório da boa-fé" in content
+    assert "inobservância do preceituado no n.º 1" in content
